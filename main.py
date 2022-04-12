42a88007b5f9c46ce400f7c7505d07bd0597a64f
Janardan Ram S <31308271+GameNameFame@users.noreply.github.com>
2022-04-12 21:29:18 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -11,27 +11,45 @@ sw, sh = pygame.display.Info().current_w, pygame.display.Info().current_h
 screen = pygame.display.set_mode((sw, sh), FULLSCREEN)
 displaysurf = pygame.Surface((800, 600))
 
+bgpic = pygame.image.load("bg.png")
+
 def writetext(position, text="Sample Text", fontsize=30, txtcolor=(0, 0, 0), txtbgcolor=None, fontttf='freesansbold'):
     fontttf += '.ttf'
     font = pygame.font.Font(fontttf, fontsize)
     txt = font.render(text, True, txtcolor, txtbgcolor)
     displaysurf.blit(txt, position)
 
-class Player:
+class Character:
     def __init__(self):
         self.rect = Rect(0, 0, 64, 128)
         self.grav = 0
         self.xacc = 0
         self.hitpoints = 100
-    def display(self):
-        pygame.draw.rect(displaysurf, (0, 0, 0), self.rect)
+
+class Player(Character):
+    def __init__(self):
+        super().__init__()
+        self.rect.x = 268
+    def display(self, scrollx):
+        pygame.draw.rect(displaysurf, (0, 0, 0), (self.rect.x - scrollx, self.rect.y, self.rect.w, self.rect.h))
+
+class Enemy(Character):
+    def __init__(self):
+        super().__init__()
+        self.rect.x = sw
+        self.rect.bottom = 440
+        self.speed = 6
+    def display(self, scrollx):
+        pygame.draw.rect(displaysurf, (200, 200, 255), (self.rect.x - scrollx, self.rect.y, self.rect.w, self.rect.h))
 
 player = Player()
+enemylist = [Enemy()]
 
 def mainloop(fresh=True):
     running = True
     if fresh:
         pass
+    scrollx = 0
     j = 0
     while j < 225:
         pygame.display.flip(); clock.tick(30)
@@ -48,6 +66,7 @@ def mainloop(fresh=True):
         j += 5
     while running:
         pygame.display.flip(); clock.tick(30)
+        scrollx += (player.rect.x - scrollx - 268)/20
         if player.rect.bottom < 440:
             player.grav += 1
         else:
@@ -65,26 +84,45 @@ def mainloop(fresh=True):
                 if ev.key == K_w:
                     if player.rect.bottom == 440:
                         player.grav = -18
+                if ev.key == K_x:
+                    print(player.rect.x)
         keys_pressed = pygame.key.get_pressed()
         if keys_pressed[K_a]:
-            if player.xacc > -10:
-                player.xacc -= 1
+            if player.rect.x > 268:
+                if player.xacc > -10:
+                    player.xacc -= 1
+            else:
+                player.xacc = 0
         elif keys_pressed[K_d]:
-            if player.xacc < 10:
-                player.xacc += 1
+            if player.rect.x < 3368:
+                if player.xacc < 10:
+                    player.xacc += 1
+            else:
+                player.xacc = 0
         else:
             if player.xacc < 0:
                 player.xacc += 1
             elif player.xacc > 0:
                 player.xacc -= 1
+        for enemy in enemylist:
+            if player.rect.right < enemy.rect.left:
+                if enemy.xacc > -enemy.speed:
+                    enemy.xacc -= 1
+            elif player.rect.left > enemy.rect.right:
+                if enemy.xacc < enemy.speed:
+                    enemy.xacc += 1
+            enemy.rect.x += enemy.xacc
         if not keys_pressed[K_LSHIFT]:
             player.rect.x += player.xacc/2
         else:
             player.rect.x += player.xacc
         player.rect.y += player.grav
         displaysurf.fill((255, 60, 0))
+        displaysurf.blit(bgpic, (-scrollx, 0))
         pygame.draw.rect(displaysurf, (0, 0, 0), (0, 600 - 160, 800, 160))
-        player.display()
+        player.display(scrollx)
+        for enemy in enemylist:
+            enemy.display(scrollx)
         screen.blit(pygame.transform.scale(displaysurf, (sw, sh)), (0, 0))
 
 def menuloop():
