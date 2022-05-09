dc53197ae6fdf515eb78aa485fdfd37f69574479
UncannyPronk <janardan.ram.sri@gmail.com>
2022-05-09 23:39:48 +0530

    main

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ wadpic = pygame.image.load("wad.png")
 
 speaking = False
 level = 0
-##level = 1
-
+level = 1
+guibool = True
 class Particle:
     def __init__(self, posx, posy, dirx, diry):
         self.size = 3
@@ -115,6 +115,9 @@ class Player(Character):
         self.attackcoords = [(0, 0), (0, 0), (-2, 1), (0, -20), (10, -70), (40, -65), (50, -30), (30, 40)]
         self.attackno = 256
         self.swordparticles = []
+        self.hitpoints = 100
+        self.attackpower = 20
+        self.hpcolor = [0, 255, 0]
     def display(self, scrollx):
         if self.direction == "right":
             if self.attack:
@@ -183,6 +186,8 @@ class Enemy(Character):
         self.attackcoords = [(0, 0), (0, 0), (-2, 1), (0, -20), (10, -70), (40, -65), (50, -30), (30, 40)]
         self.attackno = 256
         self.speed = 6
+        self.hitpoints = 100
+        self.hpcolor = [0, 255, 0]
     def display(self, scrollx):
         if self.direction == "right":
             if self.attack:
@@ -416,8 +421,6 @@ createChunk(-2400)
 createEndChunk(3200)
 createEndChunk(-3200)
 
-print(chunks)
-
 def level1():
     global level
     enemylist = [Enemy()]
@@ -428,16 +431,18 @@ def level1():
     scrollx = 0
     while running:
         keys_pressed = pygame.key.get_pressed()
+
         player.animationvar += 0.4
         if player.animationvar > 8:
             player.animationvar = 0
+
         for enemy in enemylist:
             enemy.animationvar += 0.4
             if enemy.animationvar > 8:
                 enemy.animationvar = 0
+
         pygame.display.flip(); clock.tick(30)
-        if countdown > 0:
-            countdown -= 1
+
         scrollx += (player.rect.x - scrollx - 268)/20
         # if player.rect.centerx < 585:
         #     scrollx += (player.rect.x - scrollx - 268)/20
@@ -450,11 +455,13 @@ def level1():
         for line in bglines:
             line[0][0] += 2
             line[1][0] += 2
+
         if player.rect.bottom < 440:
             player.grav += 1
         else:
             player.grav = 0
             player.rect.bottom = 440
+
         for ev in pygame.event.get():
             if ev.type == QUIT:
                 running = False
@@ -475,6 +482,7 @@ def level1():
                     print(player.rect.x)
                 if ev.key == K_z:
                     print(scrollx)
+                
         if keys_pressed[K_a]:
             if player.rect.x > -2442:
                 player.spritetype = "walk"
@@ -506,8 +514,13 @@ def level1():
             player.spritetype = "run"
             player.rect.x += player.xacc
         if player.attack:
-            if player.attackno == 2*128 and player.animationvar > 7.5:
+            if player.attackno == 2*128 and player.animationvar > 7.5 and keys_pressed[K_LSHIFT]:
                 player.grav = -12
+            if player.attackno == 3*128:
+                for enemy in enemylist:
+                    if player.rect.colliderect(enemy.rect) and player.animationvar > 7.5:
+                        enemy.hitpoints -= player.attackpower
+            
         player.rect.y += player.grav
 
         # enemy
@@ -534,6 +547,7 @@ def level1():
                 if not enemy.attack:
                     enemy.animationvar = 0
                     enemy.attackno = 3*128
+                    player.hitpoints -= 4
                 enemy.attack = True
             else:
                 enemy.rect.x += enemy.xacc
@@ -551,6 +565,19 @@ def level1():
         player.display(scrollx)
         for enemy in enemylist:
             enemy.display(scrollx)
+            enemy.hpcolor = [220 - enemy.hitpoints*2, enemy.hitpoints*2 - 20, 0]
+            for color in enemy.hpcolor:
+                if color < 0:
+                    color = 0
+            print(enemy.hpcolor)
+            pygame.draw.rect(displaysurf, (0, 0, 0), (enemy.rect.x - 2 - scrollx, enemy.rect.y - 22, 54, 14))
+            pygame.draw.rect(displaysurf, enemy.hpcolor, (enemy.rect.x - scrollx, enemy.rect.y - 20, enemy.hitpoints//2, 10))
+        pygame.draw.rect(displaysurf, (0, 0, 0), (58, 58, 204, 34))
+        player.hpcolor = [220 - player.hitpoints*2, player.hitpoints*2 - 20, 0]
+        for color in player.hpcolor:
+            if color < 0:
+                color = 0
+        pygame.draw.rect(displaysurf, player.hpcolor, (60, 60, player.hitpoints*2, 30))
         screen.blit(pygame.transform.scale(displaysurf, (sw, sh)), (0, 0))
 
 def screen_fade():
